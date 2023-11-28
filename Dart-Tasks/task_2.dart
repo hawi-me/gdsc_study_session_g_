84d464b9c779308b008c87e4f95f9bdd365714b7
hawi-me <hawiabdi41@gmail.com>
2023-11-28 23:53:38 +0300

    task two number app

---
+++ Dart-Tasks/task_2.dart
@@ -0,0 +1,154 @@
+void main() {
+  final myList = [1, 8, 3, 3, 4, 45, 5, 69, 10, 39, 91, 10];
+
+  var maximum = myList[0];
+  var minmum = myList[0];
+   var  sum = 0;
+  var n = myList.length;
+  var average;
+
+  findMaximum(List m) {
+    for (var i = 0; i < n; i++) {
+      if (myList[i] > maximum) {
+        maximum = myList[i];
+      }
+    }
+    print("maximum value in the list : $maximum");
+  }
+
+  findMinimum(List m) {
+    for (var i = 0; i < n; i++) {
+      if (myList[i] < minmum) {
+        maximum = myList[i];
+      }
+    }
+    print("minmum value in the list : $minmum");
+  }
+
+  calculateSum(List m) {
+    for (var i = 0; i < n; i++) {
+      sum = sum + myList[i];
+    }
+    print("sum:$sum");
+  }
+
+  calulateAverage(List m) {
+    //var total = calculateSum(myList);
+    for (var i = 0; i < n; i++) {
+      if (myList[i] < minmum) {
+        minmum = myList[i];
+      }
+
+      average = sum / n;
+    }
+    print("average:$average");
+  }
+
+  findMaximum(myList);
+  findMinimum(myList);
+  calculateSum(myList);
+  calulateAverage(myList);
+}
+void main() {
+  final myList = [1, 8, 3, 3, 4, 45, 5, 69, 10, 39, 91, 10];
+
+  var maximum = myList[0];
+  var minmum = myList[0];
+   var  sum = 0;
+  var n = myList.length;
+  var average;
+
+  findMaximum(List m) {
+    for (var i = 0; i < n; i++) {
+      if (myList[i] > maximum) {
+        maximum = myList[i];
+      }
+    }
+    print("maximum value in the list : $maximum");
+  }
+
+  findMinimum(List m) {
+    for (var i = 0; i < n; i++) {
+      if (myList[i] < minmum) {
+        maximum = myList[i];
+      }
+    }
+    print("minmum value in the list : $minmum");
+  }
+
+  calculateSum(List m) {
+    for (var i = 0; i < n; i++) {
+      sum = sum + myList[i];
+    }
+    print("sum:$sum");
+  }
+
+  calulateAverage(List m) {
+    //var total = calculateSum(myList);
+    for (var i = 0; i < n; i++) {
+      if (myList[i] < minmum) {
+        minmum = myList[i];
+      }
+
+      average = sum / n;
+    }
+    print("average:$average");
+  }
+
+  findMaximum(myList);
+  findMinimum(myList);
+  calculateSum(myList);
+  calulateAverage(myList);
+}
+void main() {
+  final myList = [1, 8, 3, 3, 4, 45, 5, 69, 10, 39, 91, 10];
+
+  var maximum = myList[0];
+  var minmum = myList[0];
+   var  sum = 0;
+  var n = myList.length;
+  var average;
+
+  findMaximum(List m) {
+    for (var i = 0; i < n; i++) {
+      if (myList[i] > maximum) {
+        maximum = myList[i];
+      }
+    }
+    print("maximum value in the list : $maximum");
+  }
+
+  findMinimum(List m) {
+    for (var i = 0; i < n; i++) {
+      if (myList[i] < minmum) {
+        maximum = myList[i];
+      }
+    }
+    print("minmum value in the list : $minmum");
+  }
+
+  calculateSum(List m) {
+    for (var i = 0; i < n; i++) {
+      sum = sum + myList[i];
+    }
+    print("sum:$sum");
+  }
+
+  calulateAverage(List m) {
+    //var total = calculateSum(myList);
+    for (var i = 0; i < n; i++) {
+      if (myList[i] < minmum) {
+        minmum = myList[i];
+      }
+
+      average = sum / n;
+    }
+    print("average:$average");
+  }
+
+  findMaximum(myList);
+  findMinimum(myList);
+  calculateSum(myList);
+  calulateAverage(myList);
+}
+
